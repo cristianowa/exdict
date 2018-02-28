6b00d7f174750810bae3eb85009556ec14bf6565
Cristiano Werner Araújo <cristianowerneraraujo@gmail.com>
2018-02-27 23:32:06 -0300

    Saving pypi deploy method

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+python setup.py sdist upload -r pypitest
+python setup.py sdist upload -r pypi
+# sources: https://stackoverflow.com/questions/45207128/failed-to-upload-packages-to-pypi-410-gone http://peterdowns.com/posts/first-time-with-pypi.html
