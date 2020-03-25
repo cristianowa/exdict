f0a0c84d32e04e1e766f9cda59c8bc09414a1334
Cristiano Werner Araújo <cristianowerneraraujo@gmail.com>
2020-03-25 08:32:09 -0300

    Bumb version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
         name='exdict',
         packages=find_packages(),
-        version='0.1',
+        version='0.2',
         description='Extended Dict - dictionary with attributes',
         author='Cristiano W. Araujo',
         author_email='cristianowerneraraujo@gmail.com',
